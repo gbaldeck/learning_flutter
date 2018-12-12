9647c82cf8374fcd692efbbe3942acc87c905971
gbaldeck <gbaldeck@gmail.com>
2018-12-11 22:35:51 -0700

    Changes

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,14 +3,28 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
   @override
-  State<StatefulWidget> createState() {
-    return _ProductManagerState();
-  }
+  State<StatefulWidget> createState() => _ProductManagerState();
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _products.add(widget.startingProduct);
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +33,7 @@ class _ProductManagerState extends State<ProductManager> {
         Container(
           margin: EdgeInsets.all(10.0),
           child: RaisedButton(
+            color: Theme.of(context).accentColor,
             onPressed: () {
               setState(() {
                 _products.add('Advanced Food Tester');
